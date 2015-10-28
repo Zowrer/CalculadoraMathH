01ff685e3894ea8bdd31aba9cff2db6c4965c862
ruloarias <raul.arias@ingenierosrf.mx>
2015-10-28 02:37:27 -0600

    haber si jala

--- calc.c
+++ calc.c
@@ -64,91 +64,22 @@ int main(int argc, char* argv[]){
 
                 //Con la categoria y la operacion elegidas evaluamos la funcion adecuada
                 switch (categoria) {
-<<<<<<< HEAD
-                    case 1:{
-                        suma();
-                        break;
-                    }
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5:{
-                        arcoCoseno();
-                        break;
-                    }
-                    case 6:
-                    case 7:
-                    case 8:{
-                        arcoseno();
-                        break;
-                    }
-                    case 9:{
-                        break;
-                    }
-                    case 10:{
-                        coseno();
-                    }
-                    case 11:
-                    case 12:{
-                        coship();
-                        break;
-                    }
-                    case 13:{
-                        tanhip();
-                        break;
-                    }
-                    case 14:
-                    case 15:{
-                        fmod();
-                        break:
-                    }
-                    case 16:
-                    case 17:
-                    case 18:
-                    case 19:{ potencia();
-                        break;
-                    }
-                    case 20:
-                    case 21:
-                    case 22:
-                    case 23:
-                    case 24:{
-                        frexpre();
-                        break;
-                    }
-                    case 25:{
-                        tan();
-                        break;
-                    }
-                    case 26:
-                    case 27:{
-                        printf(_("\n\nThank you for using our calculator!\n"));
-                        printf(_("Come back soon!\n\n"));
-                        break;
-                    }
-
-                    default:{
-                        printf(_("\n\nYou entered an invalid option!\n"));
-                        printf(_("Try again!\n\n"));
-                        break;
-                    }
-                }            
-=======
+            
                   case 1: { suma(); break; }
                   case 2:
                   case 3: { multiplicacion(); break; }
                   case 4:
-                  case 5:
+                  case 5:{ arcoCoseno(); break; }
                   case 6:
                   case 7:
                   case 8: { arcoseno(); break; }
                   case 9: { break; }
-                  case 10:
+                  case 10:{ coseno(); break; }
                   case 11: { senoHiper(); break; }
                   case 12: { coship(); break; }
                   case 13: { tanhip(); break; }
                   case 14: { atanDos(); break; }
-                  case 15:
+                  case 15: { fmod(); break; }
                   case 16: { logaritmoNatural(); break; }
                   case 17:
                   case 18: { raizCuadrada(); break; }
@@ -158,7 +89,7 @@ int main(int argc, char* argv[]){
                   case 22:
                   case 23: { funcionPiso(); break; }
                   case 24: { frexpre(); break; }
-                  case 25:
+                  case 25: { tan(); break;}
                   case 26:
                   case 27:{
                       printf(_("\n\nThank you for using our calculator!\n"));
